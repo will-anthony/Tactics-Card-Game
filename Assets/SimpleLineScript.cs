880bcef4b4c5aa3670ea938108c15f92f9035eba
will-anthony <will-anthony@hotmail.com>
2021-04-22 08:56:55 +0100

    LineRenderer Started

--- Assets/SimpleLineScript.cs
+++ Assets/SimpleLineScript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class SimpleLineScript : MonoBehaviour
 {
     private LineRenderer lineRenderer;
+    private bool isCardClicked;
+    private bool isLineTurnedOn;
 
     private void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        isCardClicked = false;
+        isLineTurnedOn = false;
     }
     void Start()
     {
@@ -18,16 +22,56 @@ public class SimpleLineScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !isLineTurnedOn)
         {
+            Debug.Log("MouseDown");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider.tag == "Card")
                 {
-                    lineRenderer.SetPositions(new Vector3[] { transform.position, new Vector3(2, 2, 2) });
+                    isLineTurnedOn = true;
+                    isCardClicked = true;
                 }
+            }
+        }
+
+        if(isCardClicked)
+        {
+            DisplayLine();
+        } else
+        {
+            TurnLineOff();
+        }
+
+        if(Input.GetMouseButtonUp(0) && isLineTurnedOn)
+        {
+            Debug.Log("MouseUp");
+
+            isCardClicked = false;
+
+        }
+    }
+
+    private void TurnLineOff()
+    {
+        lineRenderer.SetPositions(new Vector3[] { transform.position });
+
+    }
+
+    private void DisplayLine()
+    {
+        List<Vector3> linePoints = new List<Vector3>();
+        linePoints.Add(transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.tag == "Cube")
+            {
+                linePoints.Add(hit.transform.position);
+                lineRenderer.SetPositions(linePoints.ToArray());
             }
         }
     }
